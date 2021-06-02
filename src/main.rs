d936da88514c0d320a14905b2b7f79dbe85bbc9d
Heru Handika <herubiolog@gmail.com>
2021-06-01 21:04:07 -0500

    Add time keeping.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 mod cli;
 mod finder;
 mod merger;
+mod utils;
 
 #[macro_use]
 extern crate lazy_static;
+
+use std::time::Instant;
 
 use clap::crate_version;
 
 fn main() {
     let version = crate_version!();
+    let time = Instant::now();
     cli::parse_cli(version);
+    let duration = time.elapsed();
+    if duration.as_secs() < 60 {
+        println!("Execution time: {:?}", duration);
+    } else {
+        utils::print_formatted_duration(duration.as_secs());
+    }
 }
